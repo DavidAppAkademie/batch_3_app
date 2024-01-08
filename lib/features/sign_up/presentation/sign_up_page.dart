d96fcaacc17bf1af00902eee5272c2f255929cd1
David Sent <david.sent@app-akademie.com>
2024-01-08 12:36:41 +0100

    added navigation from sign up page

--- lib/features/sign_up/presentation/sign_up_page.dart
+++ lib/features/sign_up/presentation/sign_up_page.dart
@@ -1,4 +1,5 @@
 import 'package:batch_3_app/config/app_sizes.dart';
+import 'package:batch_3_app/features/overview/presentation/overview_page.dart';
 import 'package:flutter/material.dart';
 
 class SignUpPage extends StatelessWidget {
@@ -6,56 +7,69 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(Sizes.p16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            gapH32,
-            Center(child: Image.asset("assets/images/logo.png")),
-            gapH32,
-            const Text("E-Mail"),
-            const TextField(),
-            gapH32,
-            const Text("Passwort"),
-            const TextField(),
-            gapH32,
-            Center(
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text(
-                  "Anmelden",
-                  style: Theme.of(context).textTheme.headlineMedium,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(Sizes.p16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              gapH32,
+              Center(child: Image.asset("assets/images/logo.png")),
+              gapH32,
+              const Text("E-Mail"),
+              const TextField(
+                decoration: InputDecoration(border: OutlineInputBorder()),
+              ),
+              gapH32,
+              const Text("Passwort"),
+              const TextField(
+                decoration: InputDecoration(border: OutlineInputBorder()),
+              ),
+              gapH32,
+              Center(
+                child: ElevatedButton(
+                  onPressed: () {
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute<void>(
+                        builder: (BuildContext context) => const OverviewPage(),
+                      ),
+                    );
+                  },
+                  child: Text(
+                    "Anmelden",
+                    style: Theme.of(context).textTheme.headlineMedium,
+                  ),
                 ),
               ),
-            ),
-            gapH32,
-            Center(
-              child: TextButton(
-                onPressed: () {},
-                child: const Text("Passwort vergessen"),
-              ),
-            ),
-            gapH32,
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Expanded(child: Divider()),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: Sizes.p8),
-                  child: Text("Noch kein Konto?"),
+              gapH32,
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text("Passwort vergessen"),
                 ),
-                Expanded(child: Divider()),
-              ],
-            ),
-            gapH32,
-            Center(
-              child: TextButton(
-                onPressed: () {},
-                child: const Text("Kostenlos registrieren"),
               ),
-            ),
-          ],
+              gapH32,
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Expanded(child: Divider()),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: Sizes.p8),
+                    child: Text("Noch kein Konto?"),
+                  ),
+                  Expanded(child: Divider()),
+                ],
+              ),
+              gapH32,
+              Center(
+                child: TextButton(
+                  onPressed: () {},
+                  child: const Text("Kostenlos registrieren"),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
