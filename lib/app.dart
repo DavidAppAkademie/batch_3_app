99aae31cf3e1d606913d153223adf2cf4d05920f
David Sent <david.sent@app-akademie.com>
2024-02-05 13:12:41 +0100

    added callback function to repo for dark mode changes to apply in real time

--- lib/app.dart
+++ lib/app.dart
@@ -12,42 +12,76 @@ import 'package:batch_3_app/features/sign_up/presentation/sign_up_page.dart';
 import 'package:batch_3_app/mock_database/mock_database_service.dart';
 import 'package:flutter/material.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
   @override
-  Widget build(BuildContext context) {
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  // Passing this simulated database to all repositories
+  late final DatabaseOverviewRepository databaseOverviewRepository;
+  late final DatabaseAddFeedbackRepository databaseAddFeedbackRepository;
+  late final DatabaseAddContentRepository databaseAddContentRepository;
+  late final LocalStorageRepository localStorageRepository;
+  late Future<bool> isDarkModeFuture;
+  @override
+  void initState() {
+    super.initState();
     // Create our simulated database (Service)
+    // create service object that accesses local storage using shared prefs package
+    late final sharedPrefsService = SharedPrefsService();
     final mockDatabaseService = MockDatabaseService();
-
-    // Passing this simulated database to all repositories
-    //
-    final DatabaseOverviewRepository databaseOverviewRepository =
+    databaseOverviewRepository =
         MockDatabaseOverviewRepository(mockDatabaseService);
-
-    final DatabaseAddFeedbackRepository databaseAddFeedbackRepository =
+    databaseAddFeedbackRepository =
         MockDatabaseAddFeedbackRepository(mockDatabaseService);
-
-    final DatabaseAddContentRepository databaseAddContentRepository =
+    databaseAddContentRepository =
         MockDatabaseAddContentRepository(mockDatabaseService);
 
-    // create service object that accesses local storage using shared prefs package
-    final sharedPrefsService = SharedPrefsService();
+    localStorageRepository =
+        SharedPrefsRepository(sharedPrefsService, (newDarkMode) {
+      setState(() {
+        isDarkModeFuture = Future.value(newDarkMode);
+      });
+    });
+    isDarkModeFuture = localStorageRepository.getIsDarkMode();
+  }
 
-    // Passing this service object to the local storage repository
-    //
-    final LocalStorageRepository localStorageRepository =
-        SharedPrefsRepository(sharedPrefsService);
-
-    return MaterialApp(
-      theme: getLightTheme(),
-      darkTheme: getDarkTheme(),
-      themeMode: ThemeMode.light,
-      home: SignUpPage(
-        databaseOverviewRepository: databaseOverviewRepository,
-        databaseAddFeedbackRepository: databaseAddFeedbackRepository,
-        databaseAddContentRepository: databaseAddContentRepository,
-        localStorageRepository: localStorageRepository,
-      ),
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: isDarkModeFuture,
+      builder: (context, snapshot) {
+        if ((snapshot.connectionState == ConnectionState.waiting &&
+                !snapshot.hasData) ||
+            snapshot.hasError) {
+          // Future laedt noch.... zeige Splash Screen
+          // (oder es gab nen Fehler)
+          return const MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: CircularProgressIndicator(),
+              ),
+            ),
+          );
+        } else {
+          // Future fertig -> isDarkMode info vorhanden!
+          final isDarkMode = snapshot.data!;
+          return MaterialApp(
+            theme: getLightTheme(),
+            darkTheme: getDarkTheme(),
+            themeMode: isDarkMode ? ThemeMode.dark : ThemeMode.light,
+            home: SignUpPage(
+              databaseOverviewRepository: databaseOverviewRepository,
+              databaseAddFeedbackRepository: databaseAddFeedbackRepository,
+              databaseAddContentRepository: databaseAddContentRepository,
+              localStorageRepository: localStorageRepository,
+            ),
+          );
+        }
+      },
     );
   }
 }
--- lib/features/settings/data/shared_prefs/shared_prefs_repository.dart
+++ lib/features/settings/data/shared_prefs/shared_prefs_repository.dart
@@ -3,15 +3,17 @@ import 'package:batch_3_app/features/settings/data/shared_prefs/shared_prefs_ser
 
 class SharedPrefsRepository implements LocalStorageRepository {
   final SharedPrefsService _sharedPrefsService;
-
-  SharedPrefsRepository(this._sharedPrefsService);
+  final Function(bool) _rebuildMaterialApp;
+  SharedPrefsRepository(this._sharedPrefsService, this._rebuildMaterialApp);
   @override
   Future<bool> getIsDarkMode() {
     return _sharedPrefsService.getIsDarkmode();
   }
 
   @override
-  Future<void> setThemeMode({required bool isDarkMode}) {
-    return _sharedPrefsService.setIsDarkmode(isDarkMode);
+  Future<void> setThemeMode({required bool isDarkMode}) async {
+    await _sharedPrefsService.setIsDarkmode(isDarkMode);
+    _rebuildMaterialApp(
+        isDarkMode); // identisch mit setState Aufruf in MaterialApp
   }
 }
